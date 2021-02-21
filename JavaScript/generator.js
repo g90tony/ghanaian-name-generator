793660089b5af5654dec66bd338d93f748cbc26f
Caleb Tony <calebmbugua@gmail.com>
2021-02-21 12:56:05 +0300

    added basic states for generator logic and linked it to index html

---
+++ JavaScript/generator.js
@@ -0,0 +1,11 @@
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"];
+
+const femaleNames = ["Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+const state = {
+  gender: null,
+  birthDate: null,
+  birthDay: null,
+  name: null,
+  selectedNamesPool: [],
+};
